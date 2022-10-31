71375c8072e2b7be3104c5734ab3a447aab2c5a0
yurynefedov <yury.a.nefedov@gmail.com>
2022-10-31 20:32:42 +0400

    feat: import power-assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,4 +1,4 @@
-import { strict as assert } from 'node:assert';
+import assert from 'power-assert';
 import capitalize from '../src/capitalize.js';
 
 assert.strictEqual(capitalize('hello'), 'Hello');
